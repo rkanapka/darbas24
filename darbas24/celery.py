5abc7953c7607c90e9d9a1d9275ba00e077f76dd
RimveBiceps <kanapka.rimvydas@gmail.com>
2024-01-14 21:20:53 +0200

    ref: use pre-commit

--- darbas24/celery.py
+++ darbas24/celery.py
@@ -1,12 +1,12 @@
 from __future__ import absolute_import
-import os
-from celery import Celery
-from celery.schedules import crontab # scheduler
 
+import os
+
+from celery import Celery
 
 # default django settings
-os.environ.setdefault('DJANGO_SETTINGS_MODULE','darbas24.settings')
-app = Celery('darbas24')
-app.conf.timezone = 'UTC'
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "darbas24.settings")
+app = Celery("darbas24")
+app.conf.timezone = "UTC"
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
--- darbas24/wsgi.py
+++ darbas24/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'darbas24.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "darbas24.settings")
 
 application = get_wsgi_application()
