39c7769b879ac9207d49e1b4923174131a24573a
RimveBiceps <kanapka.rimvydas@gmail.com>
2024-01-15 21:16:13 +0200

    feat: add new fields for job offers

--- scraper/models.py
+++ scraper/models.py
@@ -3,8 +3,11 @@ from django.db import models
 
 class JobOffers(models.Model):
     title = models.CharField(max_length=200)
-    link = models.CharField(max_length=2083, default="", unique=True)
-    published = models.DateTimeField()
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    source = models.CharField(max_length=30, default="", blank=True, null=True)
+    company = models.CharField(max_length=200)
+    salary = models.CharField(max_length=50)
+    salary_period = models.CharField(max_length=25)
+    salary_calculation = models.CharField(max_length=14)
+    location = models.CharField(max_length=50)
+    job_link = models.CharField(max_length=2083, default="", unique=True)
+    image_link = models.CharField(max_length=2083, default="")
+    source_link = models.CharField(max_length=30, default="", blank=True, null=True)
