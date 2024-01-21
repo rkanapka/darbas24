a22c3800dcfa7bdc77b36646b38806969004026e
RimveBiceps <kanapka.rimvydas@gmail.com>
2024-01-21 19:55:43 +0200

    feat: scrape all offers pages

--- scraper/sites/cvbankas.py
+++ scraper/sites/cvbankas.py
@@ -9,8 +9,15 @@ class CVBankas:
 
     def scrape(self):
         try:
-            job_offers = []
-            r = requests.get(f"{self.url}/?page=1")
+            last_page_number = self.get_last_page_number()
+            return self.scrape_job_offers(last_page_number)
+        except Exception as exc:
+            print("The scraping job failed. See exception:", exc, sep="\n")
+
+    def scrape_job_offers(self, last_page_number):
+        job_offers = []
+        for page_number in range(1, last_page_number + 1):
+            r = requests.get(f"{self.url}/?page={page_number}")
             soup = BeautifulSoup(r.content, features="xml")
             articles = soup.findAll("article")
             for a in articles:
@@ -38,10 +45,7 @@ class CVBankas:
                     "" if offer_upload_date is None else offer_upload_date.text
                 )
 
-                offer_request = requests.get(job_link)
-                offer_soup = BeautifulSoup(offer_request.content, features="xml")
-                li_elements = offer_soup.find_all("li", {"class": "nav_additional_li"})
-                category = li_elements[-1].text
+                category = self.get_job_offer_category(job_link)
 
                 job_offer = {
                     "title": title,
@@ -59,6 +63,19 @@ class CVBankas:
                     "source_link": self.source,
                 }
                 job_offers.append(job_offer)
-            return job_offers
-        except Exception as exc:
-            print("The scraping job failed. See exception:", exc, sep="\n")
+        return job_offers
+
+    def get_last_page_number(self):
+        home_request = requests.get(f"{self.url}")
+        home_soup = BeautifulSoup(home_request.content, features="xml")
+        ul_pages_element = home_soup.find("ul", {"class": "pages_ul_inner"})
+        il_pages_elements = ul_pages_element.find_all("li")
+        last_page = int(il_pages_elements[-1].text)
+        return last_page
+
+    def get_job_offer_category(self, job_link):
+        offer_request = requests.get(job_link)
+        offer_soup = BeautifulSoup(offer_request.content, features="xml")
+        li_elements = offer_soup.find_all("li", {"class": "nav_additional_li"})
+        category = li_elements[-1].text
+        return category
