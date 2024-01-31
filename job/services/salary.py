206677d20df394daf33199bef9cd5f7f5f9c6855
RimveBiceps <kanapka.rimvydas@gmail.com>
2024-02-01 01:47:15 +0200

    fix: use Decimal instead of float

--- job/services/salary.py
+++ job/services/salary.py
@@ -4,17 +4,21 @@ from types import MappingProxyType
 
 class SalaryService:
     pension_rate = MappingProxyType(
-        {"not_accumulate": 0, "accumulateGradually": 0.027, "accumulate": 0.03}
+        {
+            "not_accumulate": 0,
+            "accumulateGradually": Decimal(0.027),
+            "accumulate": Decimal(0.03),
+        }
     )
     mma = 924
-    max_bruto = 2864.19
+    max_bruto = Decimal(2864.19)
 
     npd_limit = 747
     npd_bruto_limit = 2167
 
-    psd_rate = 0.0698
-    vsd_rate = 0.1252
-    gpm_rate = 0.2
+    psd_rate = Decimal(0.0698)
+    vsd_rate = Decimal(0.1252)
+    gpm_rate = Decimal(0.2)
     total_tax_rate = psd_rate + vsd_rate + gpm_rate
     post_tax_rate = 1 - total_tax_rate
 
@@ -24,24 +28,26 @@ class SalaryService:
         return self.calculate_neto(payment_amount)
 
     def calculate_neto(self, bruto):
-        npd = self.npd_limit - 0.5 * (bruto - self.mma)
+        npd = self.npd_limit - Decimal(0.5) * (bruto - self.mma)
         psd = bruto * self.psd_rate
         vsd = bruto * self.vsd_rate
         gpm = (bruto - npd) * self.gpm_rate
         return self.format_amount(bruto - psd - vsd - gpm)
 
     def calculate_bruto(self, neto, pension_type="not_accumulate"):
-        first_case_bruto = neto / (0.805 - self.pension_rate[pension_type])
+        first_case_bruto = neto / (Decimal(0.805) - self.pension_rate[pension_type])
         if first_case_bruto <= self.npd_limit:
             return self.format_amount(first_case_bruto)
 
-        second_case_bruto = (neto - 149.4) / (
+        second_case_bruto = (neto - Decimal(149.4)) / (
             self.post_tax_rate - self.pension_rate[pension_type]
         )
         if second_case_bruto <= self.mma:
             return self.format_amount(second_case_bruto)
 
-        third_case_bruto = (neto - 241.8) / (0.505 - self.pension_rate[pension_type])
+        third_case_bruto = (neto - Decimal(241.8)) / (
+            Decimal(0.505) - self.pension_rate[pension_type]
+        )
         if third_case_bruto <= self.npd_bruto_limit:
             return self.format_amount(third_case_bruto)
 
