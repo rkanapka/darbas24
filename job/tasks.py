5b78ac4d55be25de59a06fcf13af0025629fbae6
RimveBiceps <kanapka.rimvydas@gmail.com>
2024-01-31 17:18:11 +0200

    ref: refactor salary processing

--- job/tasks.py
+++ job/tasks.py
@@ -21,80 +21,72 @@ def process_job_offers(job_offers):
     print("Processing offers")
     start_time = time.time()
     try:
-        category_url_slugs_with_names = {
-            category.cvbankas_url: category.name
-            for category in JobCategory.objects.all()
-        }
+        category_slugs_with_names = get_category_slugs_with_names()
         title_max_length = JobOffer._meta.get_field("title").max_length
+        salary_service = SalaryService()
         processed_job_offers = []
-        # TODO solve edge cases: €/d. or €/val
         for offer in job_offers:
-            offer["title"] = offer["title"][:title_max_length]
-            offer["category"] = category_url_slugs_with_names.get(offer["category"])
-
-            offer["gross_pay"] = ""
-            offer["net_pay"] = ""
-
-            if offer["salary"]:
-                pay_range_start, pay_range_end, pay, pay_keyword = (
-                    None,
-                    None,
-                    None,
-                    None,
-                )
-                if "mėn" in offer["salary_period"]:
-                    if "-" in offer["salary"]:
-                        pay_range_start, pay_range_end = offer["salary"].split("-")
-                    else:
-                        pay = [int(s) for s in offer["salary"].split() if s.isdigit()][
-                            0
-                        ]
-                        pay_keyword = [
-                            str(s) for s in offer["salary"].split() if s.isalpha()
-                        ]
-                        pay_keyword = pay_keyword[0] + " " if pay_keyword else ""
-
-                    is_payment_net = (
-                        True if offer["salary_calculation"] == "Į rankas" else False
-                    )
-
-                    salary_calculation = SalaryService()
-                    if pay is not None:
-                        pay = salary_calculation.calculate_pay(pay, is_payment_net)
-                        if offer["salary_calculation"] == "Į rankas":
-                            offer["gross_pay"], offer["net_pay"] = (
-                                f"{pay_keyword}{pay}",
-                                offer["salary"],
-                            )
-                        else:
-                            offer["gross_pay"], offer["net_pay"] = (
-                                offer["salary"],
-                                f"{pay_keyword}{pay}",
-                            )
-                    else:
-                        pay_from = salary_calculation.calculate_pay(
-                            int(pay_range_start), is_payment_net
-                        )
-                        pay_to = salary_calculation.calculate_pay(
-                            int(pay_range_end), is_payment_net
-                        )
-
-                        if offer["salary_calculation"] == "Į rankas":
-                            offer["gross_pay"], offer["net_pay"] = (
-                                f"{pay_from}-{pay_to}",
-                                offer["salary"],
-                            )
-                        else:
-                            offer["gross_pay"], offer["net_pay"] = (
-                                offer["salary"],
-                                f"{pay_from}-{pay_to}",
-                            )
-
-            processed_job_offers.append(create_job_offer_object(offer))
+            processed_offer = process_single_job_offer(
+                offer, category_slugs_with_names, title_max_length, salary_service
+            )
+            processed_job_offers.append(processed_offer)
         print("Finished processing in: %s seconds" % (time.time() - start_time))
         return processed_job_offers
     except Exception as exc:
         print("Error occuread:", exc, sep="\n")
+
+
+def get_category_slugs_with_names():
+    return {
+        category.cvbankas_url: category.name for category in JobCategory.objects.all()
+    }
+
+
+def process_single_job_offer(
+    offer, category_slugs_with_names, title_max_length, salary_service
+):
+    offer["title"] = offer["title"][:title_max_length]
+    offer["category"] = category_slugs_with_names.get(offer["category"], "")
+    offer["gross_pay"], offer["net_pay"] = "", ""
+
+    if offer["salary"]:
+        process_salary(offer, salary_service)
+
+    return create_job_offer_object(offer)
+
+
+def process_salary(offer, salary_service):
+    pay_range_start, pay_range_end, pay, pay_keyword = None, None, None, None
+
+    if "mėn" in offer["salary_period"]:  # TODO solve edge cases: €/d. or €/val
+        if "-" in offer["salary"]:
+            pay_range_start, pay_range_end = offer["salary"].split("-")
+        else:
+            pay_parts = offer["salary"].split()
+            pay = next((int(s) for s in pay_parts if s.isdigit()), None)
+            pay_keyword = next((s for s in pay_parts if s.isalpha()), "")
+            pay_keyword += " " if pay_keyword else ""
+
+        is_payment_net = offer["salary_calculation"] == "Į rankas"
+
+        if pay is not None:
+            pay = salary_service.calculate_pay(pay, is_payment_net)
+            offer["gross_pay"], offer["net_pay"] = (
+                (f"{pay_keyword}{pay}", offer["salary"])
+                if offer["salary_calculation"] == "Į rankas"
+                else (offer["salary"], f"{pay_keyword}{pay}")
+            )
+        else:
+            pay_from = salary_service.calculate_pay(
+                int(pay_range_start), is_payment_net
+            )
+            pay_to = salary_service.calculate_pay(int(pay_range_end), is_payment_net)
+
+            offer["gross_pay"], offer["net_pay"] = (
+                (f"{pay_from}-{pay_to}", offer["salary"])
+                if offer["salary_calculation"] == "Į rankas"
+                else (offer["salary"], f"{pay_from}-{pay_to}")
+            )
 
 
 def save_job_offers(job_offer_objects):
