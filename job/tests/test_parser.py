2b57d4020ceab4bb2855ad375b7f50c92edf6366
RimveBiceps <kanapka.rimvydas@gmail.com>
2024-02-04 21:58:07 +0200

    test: add hourly and daily payment tests

--- job/tests/test_parser.py
+++ job/tests/test_parser.py
@@ -24,14 +24,14 @@ class TestJobParser(TestCase):
             "offer_upload_date": "prieš 21 d.",
             "source_link": "cvbankas",
         }
-
+        self.pay_keyword = "Nuo "
         self.job_offer2 = {
             "title": "Mokytojas (-a)",
             "category": "url1",
             "company": "Įmonės pavadinimas F",
-            "salary": "1200-1500",
+            "salary": f"{self.pay_keyword} 1200",
             "salary_period": "€/mėn.",
-            "salary_calculation": "Neatskaičius mokesčių",
+            "salary_calculation": "Į rankas",
             "location": "Vilniuje",
             "job_link": "https://...",
             "image_link": "https://...",
@@ -41,6 +41,37 @@ class TestJobParser(TestCase):
             "source_link": "cvbankas",
         }
 
+        self.job_offer3 = {
+            "title": "Vairuotojas (-a)",
+            "category": "url3",
+            "company": "Įmonės pavadinimas G",
+            "salary": f"{self.pay_keyword} 120",
+            "salary_period": "€/d.",
+            "salary_calculation": "Ant popieriaus",
+            "location": "Palangoje",
+            "job_link": "https://...",
+            "image_link": "https://...",
+            "image_width": 100,
+            "image_height": 30,
+            "offer_upload_date": "prieš 10 d.",
+            "source_link": "cvbankas",
+        }
+
+        self.job_offer4 = {
+            "title": "Programuotojas (-a)",
+            "category": "url4",
+            "company": "Įmonės pavadinimas H",
+            "salary": "20,5-35",
+            "salary_period": "€/val.",
+            "salary_calculation": "Į rankas",
+            "location": "Alytuje",
+            "job_link": "https://...",
+            "image_link": "https://...",
+            "image_width": 100,
+            "image_height": 30,
+            "offer_upload_date": "prieš 5 d.",
+            "source_link": "cvbankas",
+        }
         self.category_name = "Valstybinis/viešasis administravimas"
         JobCategory.objects.create(cvbankas_url="url1", name=self.category_name)
 
@@ -90,10 +121,33 @@ class TestJobParser(TestCase):
         self.assertEqual(processed_job_offers[0].title, self.job_offer1["title"])
         self.assertEqual(processed_job_offers[1].title, self.job_offer2["title"])
 
-        self.assertEqual(processed_job_offers[0].net_pay_from, Decimal("1109.89"))
-        self.assertEqual(processed_job_offers[1].net_pay_from, Decimal("847.80"))
+        self.assertEqual(processed_job_offers[1].pay_keyword, self.pay_keyword)
+        self.assertEqual(processed_job_offers[1].net_pay, Decimal("1200"))
+        self.assertEqual(processed_job_offers[1].gross_pay, Decimal("1897.43"))
 
         self.assertEqual(len(processed_job_offers), 2)
 
         for job in processed_job_offers:
             self.assertIsInstance(job, JobOffer)
+
+    def test_process_hourly_and_daily_job_offers(self):
+        processed_job_offers = self.parser.process_job_offers(
+            [self.job_offer3, self.job_offer4]
+        )
+
+        self.assertEqual(len(processed_job_offers), 2)
+
+        for job in processed_job_offers:
+            self.assertIsInstance(job, JobOffer)
+
+        self.assertEqual(processed_job_offers[0].title, self.job_offer3["title"])
+        self.assertEqual(processed_job_offers[1].title, self.job_offer4["title"])
+
+        self.assertEqual(processed_job_offers[0].pay_keyword, self.pay_keyword)
+        self.assertEqual(processed_job_offers[0].net_pay, Decimal("1514.40"))
+        self.assertEqual(processed_job_offers[0].gross_pay, Decimal("2520"))
+
+        self.assertEqual(processed_job_offers[1].gross_pay_from, Decimal("5692.56"))
+        self.assertEqual(processed_job_offers[1].gross_pay_to, Decimal("9719.01"))
+        self.assertEqual(processed_job_offers[1].net_pay_from, Decimal("3444.0"))
+        self.assertEqual(processed_job_offers[1].net_pay_to, Decimal("5880"))
